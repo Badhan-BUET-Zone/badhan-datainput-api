2ee92e90e1bf0114ed622647c7121060a9507dd7
Sanju Basak <sanjubasakndc18@gmail.com>
2022-05-06 23:41:33 +0600

    Added new route

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({
+    status: "OK",
+    message: "API is active",
+  });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+///
